579d4f5925eae1b819c5ae584c7a789940c91dd1
Paulo_Catarino <Flash_Azul@hotmail.com>
2024-03-23 08:39:26 +0000

    O meu primeiro commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("jornada de dados")
+# variaveis
+idade=34
+nome="Paulo"   # quando é variavel do tipo string devo colocar entre aspas
+print (idade)
+
+# somar
+nova_idade=idade+1
+print(nova_idade)
+
+#lista
+lista=[1,2,3,4,5]
+print(lista)
+#metodos
+lista_invertida=lista.reverse
+print(lista_invertida)
+
