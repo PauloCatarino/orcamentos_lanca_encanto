dd1c36ad01b8038f7ccfd18a6978b8e6c69b8246
PauloCatarino <flash_azul@hotmail.com>
2025-07-31 21:42:32 +0100

    217 Commit

--- dados_gerais_manager.py
+++ dados_gerais_manager.py
@@ -440,19 +440,33 @@ def importar_dados_gerais_com_opcao(parent_app, nome_tabela, mapeamento, modelo_
                     else:
                         # Busca na tabela materias_primas
                         with obter_cursor() as cursor_mp:
-                             # Seleciona apenas as colunas necessárias para atualização
-                             cursor_mp.execute("""
-                                 SELECT DESCRICAO_no_ORCAMENTO, PRECO_TABELA, PLIQ, DESC1_PLUS, DESC2_MINUS,
-                                        UND, DESP, CORESP_ORLA_0_4, CORESP_ORLA_1_0, COMP_MP, LARG_MP, ESP_MP
-                                 FROM materias_primas WHERE Ref_LE = %s
-                             """, (ref_le,))
-                             dados_atuais_mp = cursor_mp.fetchone()
-                             dados_mp_cache[ref_le] = dados_atuais_mp # Guarda no cache
+                            # Seleciona apenas as colunas necessárias para atualização
+                            cursor_mp.execute(
+                                """
+                                SELECT DESCRICAO_no_ORCAMENTO, PRECO_TABELA, PLIQ, MARGEM, DESCONTO,
+                                       UND, DESP, CORESP_ORLA_0_4, CORESP_ORLA_1_0, COMP_MP, LARG_MP, ESP_MP
+                                FROM materias_primas WHERE Ref_LE = %s
+                                """,
+                                (ref_le,))
+                            dados_atuais_mp = cursor_mp.fetchone()
+                            dados_mp_cache[ref_le] = dados_atuais_mp  # Guarda no cache
 
                 if dados_atuais_mp:
                     # Mapeia os dados de materias_primas para os nomes de campo usados no mapeamento UI
-                    map_mp_cols = ["descricao_no_orcamento", "ptab", "pliq", "desc1_plus", "desc2_minus",
-                                   "und", "desp", "corres_orla_0_4", "corres_orla_1_0", "comp_mp", "larg_mp", "esp_mp"]
+                    map_mp_cols = [
+                        "descricao_no_orcamento",
+                        "ptab",
+                        "pliq",
+                        "margem",
+                        "desconto",
+                        "und",
+                        "desp",
+                        "corres_orla_0_4",
+                        "corres_orla_1_0",
+                        "comp_mp",
+                        "larg_mp",
+                        "esp_mp",
+                    ]
                     dados_mp_dict = dict(zip(map_mp_cols, dados_atuais_mp))
 
                     # Atualiza a linha na UI com os dados de materias_primas
@@ -466,7 +480,7 @@ def importar_dados_gerais_com_opcao(parent_app, nome_tabela, mapeamento, modelo_
                                     texto_formatado = formatar_valor_moeda(valor_bd)
                                 elif campo_mp in ("comp_mp", "larg_mp", "esp_mp"):
                                     texto_formatado = f"{float(valor_bd):.1f}"
-                                elif campo_mp in ("desc1_plus", "desc2_minus", "desp"):
+                                elif campo_mp in ("MARGEM", "DESCONTO", "desp"):
                                     texto_formatado = formatar_valor_percentual(valor_bd)
                                 else:
                                     texto_formatado = str(valor_bd)
