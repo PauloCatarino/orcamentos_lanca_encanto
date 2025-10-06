cb240eb68b5d6b665b41577818dc04ff2f3f14c5
PauloCatarino <flash_azul@hotmail.com>
2025-10-06 22:40:02 +0100

    332

--- Martelo_Orcamentos_V2/run_dev.py
+++ Martelo_Orcamentos_V2/run_dev.py
@@ -30,7 +30,7 @@ git pull origin main  # Atualizar o repositório local
 # Fazer as alterações necessárias no código
 
 git add .
-git commit -m "331"
+git commit -m "332"
 git push origin main
 
 '''
